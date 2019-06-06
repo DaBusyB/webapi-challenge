436fff6c8590b36792437bb076333f308e098dc9
Brandi Apetsi <brandi@Brandis-MacBook.local>
2019-06-05 20:01:14 -0400

    projects-router get request completed

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,7 +1,21 @@
 const express = require('express')
-const Projects = require('../data/helpers/projectModel')
-const Actions = require('../data/helpers/actionModel')
+const Projects = require('../data/helpers/projectModel.js')
+const Actions = require('../data/helpers/actionModel.js')
 
 router = express.Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const  { id } = req.params 
+
+        const projects = await Projects.get(id);
+        res.status(200).json(projects);
+    } catch(error) {
+        // log error to server
+        res.status(500).json({
+            message: 'Error retrieving the projects',
+        });
+    }
+  });
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const server = express()
 
 server.use(express.json())
 server.use(helmet())
-server.use('/api/projects', projectRouter)
+server.use('/projects', projectRouter)
 
-server.get('/', (req, res, next) => {
-    const nameInsert = (req.name) ? ` ${req.name}` : '';
-  
+server.get('/', (req, res, next) => {  
     res.send(`
       <h2>Sprint Challenge for:</h2>
       <h2>Building RESTful Web APIs with Express and Node.js</h2>
