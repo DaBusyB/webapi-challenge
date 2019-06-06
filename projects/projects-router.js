c86bbe1d33d8e420e67ba0f9674e62c96fc43912
Brandi Apetsi <brandi@Brandis-MacBook.local>
2019-06-05 20:19:57 -0400

    added get actions per project id

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -16,6 +16,32 @@ router.get('/', async (req, res) => {
             message: 'Error retrieving the projects',
         });
     }
-  });
+});
+
+router.get('/:id/actions', async (req, res) => {
+    try {
+        const project = await Projects.getProjectActions(req.params.id)
+
+        if(project) {
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({ message: 'Project not found' })
+        }
+    } catch(err) { 
+        res.status(500).json({ message: 'Error retrieving the project' })
+    }
+})
+
+router.put('/', async (req, res) => {
+    
+})
+
+router.post('/', async (req, res) => {
+
+})
+
+router.delete('/', async (req, res) => {
+
+})
 
 module.exports = router
