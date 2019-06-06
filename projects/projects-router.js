f4e97f4a11aac011db300a75b44570f02d401d5d
Brandi Apetsi <brandi@Brandis-MacBook.local>
2019-06-05 21:06:14 -0400

    added post for action per project id

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -34,7 +34,7 @@ router.put('/:id', async (req, res) => {
 
         res.status(200).json(project)
     } catch(error) {
-        res.status(500).json({ message: 'Error editing the hub' })
+        res.status(500).json({ message: 'Error editing the project' })
     }
 })
 
@@ -42,11 +42,13 @@ router.delete('/:id', async (req, res) => {
     try {
         const project =  await Projects.remove(req.params.id)
 
-        res.status(200).json({ message: 'The hub has been deleted.' })
+        res.status(200).json({ message: 'The project has been deleted.' })
     } catch(error) {
-        res.status(500).json({ message: 'Error removing the hub' })
+        res.status(500).json({ message: 'Error removing the project' })
     }
 })
+
+// ******** Actions Sub-routes ********
 
 router.get('/:id/actions', async (req, res) => {
     try {
@@ -58,7 +60,40 @@ router.get('/:id/actions', async (req, res) => {
             res.status(404).json({ message: 'Project not found' })
         }
     } catch(err) { 
-        res.status(500).json({ message: 'Error retrieving the project' })
+        res.status(500).json({ message: 'Error retrieving the actions' })
     }
 })
+
+router.post('/:id/actions', async (req, res) => {
+    const actionInfo = {...req.body, project_id: req.params.id}
+
+    try {
+        const action = await Actions.insert(actionInfo)
+
+        res.status(201).json(action)
+    } catch(error) {
+        res.status(500).json({ message: 'Error adding the action' })
+    }
+})
+
+router.put('/:id/actions/:id', async (req, res) => {
+    try {
+        const action = await Actions.update(req.params.id, req.body)
+
+        res.status(200).json(action)
+    } catch(error) {
+        res.status(500).json({ message: 'Error editing the action' })
+    }
+})
+
+router.delete('/:id/actions/:id', async (req, res) => {
+    try {
+        const action =  await Actions.remove(req.params.id)
+
+        res.status(200).json({ message: 'The action has been deleted.' })
+    } catch(error) {
+        res.status(500).json({ message: 'Error removing the action' })
+    }
+})
+
 module.exports = router
